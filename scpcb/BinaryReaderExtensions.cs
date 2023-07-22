bee6e5e2a5f172632d662943f6221095d306edf1
Salvage <29021710+Saalvage@users.noreply.github.com>
2023-07-22 04:20:17 +0200

    Optimized `ReadB3DString` impl

--- scpcb/BinaryReaderExtensions.cs
+++ scpcb/BinaryReaderExtensions.cs
@@ -4,15 +4,63 @@ using System.Text;
 namespace scpcb; 
 
 public static class BinaryReaderExtensions {
+    [ThreadStatic]
+    private static WeakReference<StringBuilder> _sb;
+
     /// <summary>
     /// ASCII String prefixed by a 32-bit integer length.
     /// </summary>
     /// <param name="reader"></param>
     /// <returns></returns>
     public static string ReadB3DString(this BinaryReader reader) {
-        var length = reader.ReadInt32();
-        // TODO: Optimize if needed, see ReadString impl.
-        return Encoding.ASCII.GetString(reader.ReadBytes(length));
+        // Implementation stolen from BinaryReader.ReadString()
+        // Only difference is only ASCII support so byte count == char count
+
+        var stringLength = reader.ReadInt32();
+
+        switch (stringLength) {
+            case < 0:
+                throw new IOException("Failed to read B3D string!");
+            case 0:
+                return "";
+        }
+
+        var bufferSize = Math.Min(stringLength, 128);
+        Span<byte> charBytes = stackalloc byte[bufferSize];
+        Span<char> chars = stackalloc char[bufferSize];
+
+        var currPos = 0;
+        StringBuilder sb = null!;
+        do {
+            var readLength = ((stringLength - currPos) > bufferSize) ? bufferSize : (stringLength - currPos);
+
+            var n = reader.Read(charBytes[..readLength]);
+            if (n == 0) {
+                throw new IOException("EOF encountered while reading B3D string!");
+            }
+
+            Encoding.ASCII.GetChars(charBytes, chars);
+
+            if (currPos == 0) {
+                if (n == stringLength) {
+                    return new(chars[..n]);
+                } else {
+                    // Since we could be reading from an untrusted data source, limit the initial size of the
+                    // StringBuilder instance we're about to get or create. It'll expand automatically as needed.
+                    if (!_sb?.TryGetTarget(out sb) ?? true) {
+                        sb = new(Math.Min(stringLength, 360));
+                        _sb = new(sb);
+                    }
+                }
+            }
+
+            sb.Append(chars[..n]);
+            currPos += n;
+        } while (currPos < stringLength);
+
+        var str = sb.ToString();
+        sb.Clear();
+        return str;
     }
 
     public static Vector2 ReadVector2(this BinaryReader reader)
