1fba7bba13c952d66f7f0ca283b8cee708f60c48
Salvage <29021710+Saalvage@users.noreply.github.com>
2023-07-29 06:07:13 +0200

    Fix transform matrix

--- scpcb/Transform.cs
+++ scpcb/Transform.cs
@@ -6,7 +6,7 @@ public record struct Transform(Vector3 Position, Quaternion Rotation, Vector3 Sc
     public Transform() : this(Vector3.Zero, Quaternion.Identity, Vector3.One) { }
 
     public Matrix4x4 GetMatrix()
-        => Matrix4x4.CreateTranslation(Position)
-         * Matrix4x4.CreateFromQuaternion(Rotation)
-         * Matrix4x4.CreateScale(Scale);
+        => Matrix4x4.CreateScale(Scale)
+         * Matrix4x4.CreateTranslation(Position)
+         * Matrix4x4.CreateFromQuaternion(Rotation);
 }
