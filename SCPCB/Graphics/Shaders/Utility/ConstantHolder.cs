39194946000147e99ea6a9e0483ee49bcac0a5e3
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-01-01 04:25:25 +0100

    Optimize dirty check for constant buffers

--- SCPCB/Graphics/Shaders/Utility/ConstantHolder.cs
+++ SCPCB/Graphics/Shaders/Utility/ConstantHolder.cs
@@ -98,6 +98,149 @@ public class ConstantHolder<TVertConstants, TFragConstants> : Disposable, IConst
         }
     }
 
+    // Taken from SpanHelpers.SequenceEqual, the jump magic genuinely improves performance.
+    // We put the small cases at the very end because small sizes are highly unusual for constant buffers.
+    // We also ignore a special case for >=64 (but <=128) bits on 64 bit machines with 128 vector support.
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    private static unsafe bool SequenceEqual(ref byte first, ref byte second, nuint length) {
+        bool result;
+        // Use nint for arithmetic to avoid unnecessary 64->32->64 truncations
+        if (Vector128.IsHardwareAccelerated) {
+            if (Vector512.IsHardwareAccelerated && length >= (nuint)Vector512<byte>.Count) {
+                nuint offset = 0;
+                nuint lengthToExamine = length - (nuint)Vector512<byte>.Count;
+                // Unsigned, so it shouldn't have overflowed larger than length (rather than negative)
+                Debug.Assert(lengthToExamine < length);
+                if (lengthToExamine != 0) {
+                    do {
+                        if (Vector512.LoadUnsafe(ref first, offset) !=
+                            Vector512.LoadUnsafe(ref second, offset)) {
+                            goto NotEqual;
+                        }
+                        offset += (nuint)Vector512<byte>.Count;
+                    } while (lengthToExamine > offset);
+                }
+
+                // Do final compare as Vector512<byte>.Count from end rather than start
+                if (Vector512.LoadUnsafe(ref first, lengthToExamine) ==
+                    Vector512.LoadUnsafe(ref second, lengthToExamine)) {
+                    // C# compiler inverts this test, making the outer goto the conditional jmp.
+                    goto Equal;
+                }
+
+                // This becomes a conditional jmp forward to not favor it.
+                goto NotEqual;
+            } else if (Vector256.IsHardwareAccelerated && length >= (nuint)Vector256<byte>.Count) {
+                nuint offset = 0;
+                nuint lengthToExamine = length - (nuint)Vector256<byte>.Count;
+                // Unsigned, so it shouldn't have overflowed larger than length (rather than negative)
+                Debug.Assert(lengthToExamine < length);
+                if (lengthToExamine != 0) {
+                    do {
+                        if (Vector256.LoadUnsafe(ref first, offset) !=
+                            Vector256.LoadUnsafe(ref second, offset)) {
+                            goto NotEqual;
+                        }
+                        offset += (nuint)Vector256<byte>.Count;
+                    } while (lengthToExamine > offset);
+                }
+
+                // Do final compare as Vector256<byte>.Count from end rather than start
+                if (Vector256.LoadUnsafe(ref first, lengthToExamine) ==
+                    Vector256.LoadUnsafe(ref second, lengthToExamine)) {
+                    // C# compiler inverts this test, making the outer goto the conditional jmp.
+                    goto Equal;
+                }
+
+                // This becomes a conditional jmp forward to not favor it.
+                goto NotEqual;
+            } else if (length >= (nuint)Vector128<byte>.Count) {
+                nuint offset = 0;
+                nuint lengthToExamine = length - (nuint)Vector128<byte>.Count;
+                // Unsigned, so it shouldn't have overflowed larger than length (rather than negative)
+                Debug.Assert(lengthToExamine < length);
+                if (lengthToExamine != 0) {
+                    do {
+                        if (Vector128.LoadUnsafe(ref first, offset) !=
+                            Vector128.LoadUnsafe(ref second, offset)) {
+                            goto NotEqual;
+                        }
+                        offset += (nuint)Vector128<byte>.Count;
+                    } while (lengthToExamine > offset);
+                }
+
+                // Do final compare as Vector128<byte>.Count from end rather than start
+                if (Vector128.LoadUnsafe(ref first, lengthToExamine) ==
+                    Vector128.LoadUnsafe(ref second, lengthToExamine)) {
+                    // C# compiler inverts this test, making the outer goto the conditional jmp.
+                    goto Equal;
+                }
+
+                // This becomes a conditional jmp forward to not favor it.
+                goto NotEqual;
+            }
+        }
+
+        // We don't implement any of the special cases for small lengths as these are highly untypical
+        // of constant buffers (we're talking less than a Vector4 here)
+        if (length >= (nuint)sizeof(nuint)) {
+            nuint offset = 0;
+            nuint lengthToExamine = length - (nuint)sizeof(nuint);
+            // Unsigned, so it shouldn't have overflowed larger than length (rather than negative)
+            Debug.Assert(lengthToExamine < length);
+            if (lengthToExamine > 0) {
+                do {
+                    // Compare unsigned so not do a sign extend mov on 64 bit
+                    if (Unsafe.ReadUnaligned<nuint>(ref Unsafe.AddByteOffset(ref first, offset))
+                        != Unsafe.ReadUnaligned<nuint>(ref Unsafe.AddByteOffset(ref second, lengthToExamine))) {
+                        goto NotEqual;
+                    }
+                    offset += (nuint)sizeof(nuint);
+                } while (lengthToExamine > offset);
+            }
+
+            // Do final compare as sizeof(nuint) from end rather than start
+            result = (Unsafe.ReadUnaligned<nuint>(ref Unsafe.AddByteOffset(ref first, lengthToExamine))
+                      == Unsafe.ReadUnaligned<nuint>(ref Unsafe.AddByteOffset(ref second, lengthToExamine)));
+            goto Result;
+        }
+
+        if (length < sizeof(uint)) {
+            uint differentBits = 0;
+            nuint offset = (length & 2);
+            if (offset != 0) {
+                differentBits = Unsafe.ReadUnaligned<ushort>(ref first);
+                differentBits -= Unsafe.ReadUnaligned<ushort>(ref second);
+            }
+            if ((length & 1) != 0) {
+                differentBits |= (uint)Unsafe.AddByteOffset(ref first, offset) - (uint)Unsafe.AddByteOffset(ref second, offset);
+            }
+            result = (differentBits == 0);
+            goto Result;
+        } else {
+            nuint offset = length - sizeof(uint);
+            uint differentBits = Unsafe.ReadUnaligned<uint>(ref first) - Unsafe.ReadUnaligned<uint>(ref second);
+            differentBits |= Unsafe.ReadUnaligned<uint>(ref Unsafe.AddByteOffset(ref first, offset)) - Unsafe.ReadUnaligned<uint>(ref Unsafe.AddByteOffset(ref second, offset));
+            result = (differentBits == 0);
+            goto Result;
+        }
+
+    Result:
+        return result;
+        // When the sequence is equal; which is the longest execution, we want it to determine that
+        // as fast as possible so we do not want the early outs to be "predicted not taken" branches.
+    Equal:
+        return true;
+
+        // As there are so many true/false exit points the Jit will coalesce them to one location.
+        // We want them at the end so the conditional early exit jmps are all jmp forwards so the
+        // branch predictor in a uninitialized state will not take them e.g.
+        // - loops are conditional jmps backwards and predicted
+        // - exceptions are conditional forwards jmps and not predicted
+    NotEqual:
+        return false;
+    }
+
     public void UpdateAndSetBuffers(CommandList commands, uint index) {
         if (_isDirty) {
             UpdateBuffer(commands, _vertexBuffer, ref _vert, ref _lastVert, !_hasEverUpdated);
@@ -110,8 +253,12 @@ public class ConstantHolder<TVertConstants, TFragConstants> : Disposable, IConst
         commands.SetGraphicsResourceSet(index, _set);
 
         // This is a pretty hot path (on avg. 1 invocation per shader per frame) so further optimizations might be warranted.
-        // See Span's SequenceEqual.
-        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        // 1. We used to calculate the exact portion of the buffer that needs to be updated. Adjusting the SequenceEquals
+        // to return the offset where changes begin yielded negative results, likely due to severe pessimization of the SequenceEquals.
+        // It might make sense to have another crack at this, since we should get the information *where* we don't change for "free"
+        // (at least at the start). To be noted that either way we'd be trading more CPU time for less GPU time.
+        // 2. Making it so constant buffers "know" the best way (their most appropriate vector size) to compare themselves should be
+        // possible using static interfaces and would allow for cutting down on the branching, likely at the cost of more indirection.
         static unsafe void UpdateBuffer<T>(CommandList commands, DeviceBuffer? buffer, ref T curr, ref T prev, bool force) where T : unmanaged {
             if (buffer == null) {
                 return;
@@ -123,31 +270,9 @@ public class ConstantHolder<TVertConstants, TFragConstants> : Disposable, IConst
                 return;
             }
 
-            fixed (T* currPtr = &curr) {
-                fixed (T* prevPtr = &prev) {
-                    var currBytes = (byte*)currPtr;
-                    var prevBytes = (byte*)prevPtr;
-
-                    var offset = 0;
-                    for (; offset < sizeof(T); offset++) {
-                        if (currBytes[offset] != prevBytes[offset]) {
-                            break;
-                        }
-                    }
-
-                    var differentUntil = sizeof(T) - 1;
-                    for (; differentUntil > offset; differentUntil--) {
-                        if (currBytes[differentUntil] != prevBytes[differentUntil]) {
-                            break;
-                        }
-                    }
-
-                    if (offset != sizeof(T)) {
-                        var dataToUpdate = new Span<byte>(currBytes + offset, differentUntil - offset + 1);
-                        commands.UpdateBuffer(buffer, (uint)offset, dataToUpdate);
-                        prev = curr;
-                    }
-                }
+            if (!SequenceEqual(ref Unsafe.As<T, byte>(ref curr), ref Unsafe.As<T, byte>(ref prev), (nuint)sizeof(T))) {
+                commands.UpdateBuffer(buffer, 0, ref curr);
+                prev = curr;
             }
         }
     }
