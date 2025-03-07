6666f196254d5ab4d99634cf1ee45c046c041ae6
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-03-07 11:29:06 +0100

    Replace NVorbis with more modern fork

--- SCPCB/Audio/NVorbisReader.cs
+++ SCPCB/Audio/NVorbisReader.cs
@@ -9,15 +9,17 @@ public class NVorbisReader : Disposable, ISampleProvider {
 
     public WaveFormat WaveFormat { get; }
 
+    // In bytes.
     public long Length => _vorbisReader.TotalSamples * WaveFormat.BitsPerSample / 8 * WaveFormat.Channels;
 
     public NVorbisReader(string path) {
         _vorbisReader = new(path);
+        _vorbisReader.Initialize();
         WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(_vorbisReader.SampleRate, _vorbisReader.Channels);
     }
 
     public int Read(float[] buffer, int offset, int count) {
-        return _vorbisReader.ReadSamples(buffer.AsSpan()[offset..(offset + count)]);
+        return _vorbisReader.ReadSamples(buffer.AsSpan()[offset..(offset + count)]) * WaveFormat.Channels;
     }
 
     protected override void DisposeImpl() {
